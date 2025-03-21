2febdef6a6b45683f4bd0747df7886a6969a2b66
PixelPioneer <150123454+JaydeepRokade@users.noreply.github.com>
2025-03-21 23:15:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const { ApolloServer, gql } = require('apollo-server-express');
 const mongoose = require('mongoose');
 require('dotenv').config(); // To load environment variables
 
-// Connect to MongoDB
+// Connect to MongoDB of of Toronto for over rto
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
